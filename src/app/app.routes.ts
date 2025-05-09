02605f05e95eaaf4e1ae28032fc354b63e9040c7
Daniel Coates <dcoates@scottlogic.com>
2025-05-09 12:55:24 +0100

    about page for future update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { HomeComponent } from './pages/home/home.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { GurveerComponent } from './pages/gurveer/gurveer.component';
 import { CreateAccountComponent } from './pages/create-account/create-account.component';
+import { AboutComponent } from './pages/about/about.component';
 
 
 export const routes: Routes = [
@@ -10,5 +11,6 @@ export const routes: Routes = [
     {path: "pages/home", component: HomeComponent},
     {path: "pages/gurveer", component: GurveerComponent},
     {path: "pages/create-account", component: CreateAccountComponent},
+    {path: "pages/about", component: AboutComponent},
     {path: "**", component: NotFoundComponent}
 ];
