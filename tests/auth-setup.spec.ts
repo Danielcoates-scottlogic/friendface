07b756abba4641fd72e1756f91eb461c4f0fa84c
Daniel Coates <dcoates@scottlogic.com>
2025-05-28 11:09:04 +0100

    prettier

--- tests/auth-setup.spec.ts
+++ tests/auth-setup.spec.ts
@@ -1,13 +1,13 @@
 import { test, expect } from '@playwright/test';
 //auth expires after 30 mins
 test('login and save storage', async ({ page }) => {
-    await page.goto('http://localhost:4200/');
+  await page.goto('http://localhost:4200/');
 
-    await page.getByPlaceholder('Username').fill('dan');
-    await page.getByPlaceholder('Password').fill('dan');
-    const button = page.locator('input[type="submit"]');
-    await button.click();
+  await page.getByPlaceholder('Username').fill('dan');
+  await page.getByPlaceholder('Password').fill('dan');
+  const button = page.locator('input[type="submit"]');
+  await button.click();
 
-    await expect(page).toHaveURL('http://localhost:4200/pages/home');
-    await page.context().storageState({ path: 'storage/auth.json' });
+  await expect(page).toHaveURL('http://localhost:4200/pages/home');
+  await page.context().storageState({ path: 'storage/auth.json' });
 });
--- tests/create_account.spec.ts
+++ tests/create_account.spec.ts
@@ -8,7 +8,6 @@ test('has title', async ({ page }) => {
 test('login with valid user test', async ({ page }) => {
   await page.goto('http://localhost:4200/');
 
-
   await page.getByPlaceholder('Username').fill('dan');
   await page.getByPlaceholder('Password').fill('dan');
   const button = page.locator('input[type="submit"]');
@@ -21,11 +20,10 @@ test('login with valid user test', async ({ page }) => {
 test('login with invalid user test', async ({ page }) => {
   await page.goto('http://localhost:4200/');
 
-
   await page.getByPlaceholder('Username').fill('Not real');
   await page.getByPlaceholder('Password').fill('Not real');
 
-  page.once('dialog', async dialog => {
+  page.once('dialog', async (dialog) => {
     expect(dialog.message()).toBe('Invalid credentials.');
     await dialog.dismiss();
   });
@@ -37,11 +35,10 @@ test('login with invalid user test', async ({ page }) => {
 test('login with username less than 3 characters', async ({ page }) => {
   await page.goto('http://localhost:4200/');
 
-
   await page.getByPlaceholder('Username').fill('No');
   await page.getByPlaceholder('Password').fill('Not real');
 
-  page.once('dialog', async dialog => {
+  page.once('dialog', async (dialog) => {
     expect(dialog.message()).toBe('Username must be at least 3 characters');
     await dialog.dismiss();
   });
@@ -53,11 +50,10 @@ test('login with username less than 3 characters', async ({ page }) => {
 test('login with password less than 3 characters', async ({ page }) => {
   await page.goto('http://localhost:4200/');
 
-
   await page.getByPlaceholder('Username').fill('Not real');
   await page.getByPlaceholder('Password').fill('No');
 
-  page.once('dialog', async dialog => {
+  page.once('dialog', async (dialog) => {
     expect(dialog.message()).toBe('password must be at least 3 characters');
     await dialog.dismiss();
   });
@@ -68,71 +64,75 @@ test('login with password less than 3 characters', async ({ page }) => {
 
 test('create account, username taken', async ({ page }) => {
   await page.goto('http://localhost:4200/');
-  await page.getByRole('button', { name: /not got an account\? create one/i }).click();
+  await page
+    .getByRole('button', { name: /not got an account\? create one/i })
+    .click();
   await page.getByPlaceholder('Enter a username').fill('dan');
   await page.getByPlaceholder('Enter a password').fill('dan');
   await page.getByPlaceholder('Re-enter your password').fill('dan');
 
-  page.once('dialog', async dialog => {
+  page.once('dialog', async (dialog) => {
     expect(dialog.message()).toBe('User already exists!');
     await dialog.dismiss();
   });
 
   await page.locator('input[type="submit"]');
-
-})
+});
 
 test('create account, passwords dont match', async ({ page }) => {
   await page.goto('http://localhost:4200/');
-  await page.getByRole('button', { name: /not got an account\? create one/i }).click();
+  await page
+    .getByRole('button', { name: /not got an account\? create one/i })
+    .click();
   await page.getByPlaceholder('Enter a username').fill('dan');
   await page.getByPlaceholder('Enter a password').fill('dan');
   await page.getByPlaceholder('Re-enter your password').fill('da');
 
-  page.once('dialog', async dialog => {
-    expect(dialog.message()).toBe('Passwords don\'t match');
+  page.once('dialog', async (dialog) => {
+    expect(dialog.message()).toBe("Passwords don't match");
     await dialog.dismiss();
   });
 
   await page.locator('input[type="submit"]');
-
-})
+});
 
 test('create account, username less than 3 chars', async ({ page }) => {
   await page.goto('http://localhost:4200/');
-  await page.getByRole('button', { name: /not got an account\? create one/i }).click();
+  await page
+    .getByRole('button', { name: /not got an account\? create one/i })
+    .click();
   await page.getByPlaceholder('Enter a username').fill('da');
   await page.getByPlaceholder('Enter a password').fill('dan');
   await page.getByPlaceholder('Re-enter your password').fill('da');
 
-  page.once('dialog', async dialog => {
+  page.once('dialog', async (dialog) => {
     expect(dialog.message()).toBe('Username must be at least 3 characters');
     await dialog.dismiss();
   });
 
   await page.locator('input[type="submit"]');
-
-})
+});
 
 test('create account, password less than 3 chars', async ({ page }) => {
   await page.goto('http://localhost:4200/');
-  await page.getByRole('button', { name: /not got an account\? create one/i }).click();
+  await page
+    .getByRole('button', { name: /not got an account\? create one/i })
+    .click();
   await page.getByPlaceholder('Enter a username').fill('dan');
   await page.getByPlaceholder('Enter a password').fill('da');
   await page.getByPlaceholder('Re-enter your password').fill('da');
 
-  page.once('dialog', async dialog => {
+  page.once('dialog', async (dialog) => {
     expect(dialog.message()).toBe('password must be at least 3 characters');
     await dialog.dismiss();
   });
 
   await page.locator('input[type="submit"]');
-
-})
+});
 
 test('dark mode toggle', async ({ page }) => {
   await page.goto('http://localhost:4200/');
   const icon = page.locator('button mat-icon');
   await icon.click();
   await expect(page.locator('body')).toHaveClass('dark-theme');
-})
+});
